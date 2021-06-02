420587971c5465c201b2b5583528285a43d91d77
Elijah Ally <elijahally18@gmail.com>
2021-06-01 20:27:52 -0400

    finished userAuth except components

--- frontend/reducers/entities_reducer.js
+++ frontend/reducers/entities_reducer.js
@@ -1,10 +1,8 @@
 import { combineReducers } from 'redux';
 import userReducer from './user_reducer';
-import sessionReducer from './session_reducer';
 
 const entities = combineReducers({
 	user: userReducer,
-	session: sessionReducer,
 });
 
 export default entities;
